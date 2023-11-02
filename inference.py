9791dc4b1ece550cd958cbbb6f1c51c49a28ce6d
Sang Han <sanghan@protonmail.com>
2023-11-01 20:35:00 -0700

    implement super resolution

--- inference.py
+++ inference.py
@@ -1,90 +1,96 @@
 import os
 import argparse
 import sys
+import cv2
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 from colorizator import MangaColorizator
+from superres import upscale_images, upscale_single_image
+
 
 def process_image(image, colorizator, args):
     colorizator.set_image(image, args.size, args.denoiser, args.denoiser_sigma)
-        
-    return colorizator.colorize()
-    
-def colorize_single_image(image_path, save_path, colorizator, args):
-    
-        image = plt.imread(image_path)
 
-        colorization = process_image(image, colorizator, args)
-        
-        plt.imsave(save_path, colorization)
-        
-        return True
-    
+    return colorizator.colorize()
+
+
+def colorize_single_image(image_path, save_path, colorizator, args):
+    image = plt.imread(image_path)
+
+    colorization = process_image(image, colorizator, args)
+
+    plt.imsave(save_path, colorization)
+
+    return True
+
 
 def colorize_images(target_path, colorizator, args):
     images = os.listdir(args.path)
-    
+
     for image_name in images:
         file_path = os.path.join(args.path, image_name)
-        
+
         if os.path.isdir(file_path):
             continue
-        
+
         name, ext = os.path.splitext(image_name)
-        if (ext != '.png'):
-            image_name = name + '.png'
-        
+        if ext != ".png":
+            image_name = name + ".png"
+
         print(file_path)
-        
+
         save_path = os.path.join(target_path, image_name)
         colorize_single_image(file_path, save_path, colorizator, args)
-    
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--path", required=True)
-    parser.add_argument("-gen", "--generator", default = 'networks/generator.zip')
-    parser.add_argument("-ext", "--extractor", default = 'networks/extractor.pth')
-    parser.add_argument('-g', '--gpu', dest = 'gpu', action = 'store_true')
-    parser.add_argument('-nd', '--no_denoise', dest = 'denoiser', action = 'store_false')
-    parser.add_argument("-ds", "--denoiser_sigma", type = int, default = 25)
-    parser.add_argument("-s", "--size", type = int, default = 576)
-    parser.set_defaults(gpu = False)
-    parser.set_defaults(denoiser = True)
+    parser.add_argument("-gen", "--generator", default="networks/generator.zip")
+    parser.add_argument("-ext", "--extractor", default="networks/extractor.pth")
+    parser.add_argument("-g", "--gpu", dest="gpu", action="store_true")
+    parser.add_argument("-nd", "--no_denoise", dest="denoiser", action="store_false")
+    parser.add_argument("-ds", "--denoiser_sigma", type=int, default=25)
+    parser.add_argument("-s", "--size", type=int, default=576)
+    parser.add_argument("-up", "--upscale", action="store_true")
+    parser.set_defaults(gpu=False)
+    parser.set_defaults(denoiser=True)
     args = parser.parse_args()
-    
+
     return args
 
-    
+
 if __name__ == "__main__":
-    
     args = parse_args()
-    
+
     if args.gpu:
-        device = 'cuda'
+        device = "cuda"
     else:
-        device = 'cpu'
-        
+        device = "cpu"
+
     colorizer = MangaColorizator(device, args.generator, args.extractor)
-    
+
     if os.path.isdir(args.path):
-        colorization_path = os.path.join(args.path, 'colorization')
+        colorization_path = os.path.join(args.path, "colorization")
         if not os.path.exists(colorization_path):
             os.makedirs(colorization_path)
-              
+
         colorize_images(colorization_path, colorizer, args)
-        
+        if args.upscale:
+            upscale_images(colorization_path)
+
     elif os.path.isfile(args.path):
-        
         split = os.path.splitext(args.path)
-        
-        if split[1].lower() in ('.jpg', '.png', ',jpeg'):
-            new_image_path = split[0] + '_colorized' + '.png'
-            
+
+        if split[1].lower() in (".jpg", ".png", ",jpeg"):
+            new_image_path = split[0] + "_colorized" + ".png"
+
             colorize_single_image(args.path, new_image_path, colorizer, args)
+            if args.upscale:
+                upscale_single_image(new_image_path)
         else:
-            print('Wrong format')
+            print("Wrong format")
     else:
-        print('Wrong path')
-    
+        print("Wrong path")
---
+++ superres.py
@@ -0,0 +1,59 @@
+import cv2
+import os
+
+from urllib.request import urlretrieve
+
+
+def get_dnn_superres(upscale_factor: int = 3):
+    """
+    Returns a DNN Super Resolution model for the given upscale factor.
+
+    Parameters
+    ----------
+    upscale_factor: int
+        Upscale factor for the model (only 2x, 3x, and 4x are supported)
+
+    Returns
+    -------
+    cv2.dnn_superres.DnnSuperResImpl
+        Super Resolution Model
+    """
+    if upscale_factor not in range(2, 5):
+        raise ValueError(
+            f"Upscale factor must be between 2 and 4, got {upscale_factor}"
+        )
+
+    # Download the pretrained model
+    filename = f"ESPCN_x{upscale_factor}.pb"
+    if not os.path.exists(f"/tmp/{filename}"):
+        urlretrieve(
+            f"https://github.com/fannymonori/TF-ESPCN/raw/master/export/{filename}",
+            f"/tmp/{filename}",
+        )
+
+    sr = cv2.dnn_superres.DnnSuperResImpl().create()
+    sr.readModel(f"/tmp/{filename}")
+    sr.setModel("espcn", upscale_factor)
+
+    return sr
+
+
+def upscale_images(target_path):
+    images = os.listdir(target_path)
+
+    for image_name in images:
+        file_path = os.path.join(target_path, image_name)
+
+        if os.path.isdir(file_path):
+            continue
+
+        upscale_single_image(file_path)
+
+
+def upscale_single_image(image_path):
+    img = cv2.imread(image_path)
+    result = SR.upsample(img)
+    cv2.imwrite(image_path, result)
+
+
+SR = get_dnn_superres(upscale_factor=2)
